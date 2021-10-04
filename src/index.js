73dfef68d4138ed13c5df7f86c8ffc7089af9a64
Alisson <thelinuxlich@gmail.com>
2021-10-04 19:40:32 -0300

    remove rubico for now

--- src/index.js
+++ src/index.js
@@ -1,48 +1,41 @@
 import Redis from "ioredis"
 import * as R from "ramda"
-import rubico from "rubico"
-
-const { map, tryCatch, pipe, reduce, curry, filter } = rubico
 
 export default (host = "localhost", port = 6379) => {
   const redis = new Redis(port, host)
   const STANDARD_STATUSES = ["complete", "trigger"]
   const CONSUMERGROUP = "consumers"
   const CONSUMERNAME = "consumer"
-  const STREAMARGS = () => map((_) => ">")(Array(STREAMS.length))
+  const STREAMARGS = () => Array(STREAMS.length).fill(">")
 
   const addStreams = (type, atom) =>
-    map((s) => `${type}:${atom}:${s}`)(STANDARD_STATUSES)
+    STANDARD_STATUSES.map((s) => `${type}:${atom}:${s}`)
 
   const createGroup = (stream) =>
     redis.xgroup("CREATE", stream, CONSUMERGROUP, 0, "MKSTREAM")
 
-  const getGroups = tryCatch(
-    (stream) =>
-      pipe(
-        curry(redis.xinfo("GROUPS")),
-        filter((g) => g[1] === CONSUMERGROUP),
-        switchCase(
-          (groups) => groups.length > 1,
-          (groups) => groups,
-          (_) => createGroup(stream)
-        )
-      )(stream),
-    createGroup
-  )
-
-  const arrayToPairs = reduce((result, _, index, array) => {
-    if (index % 2 === 0) {
-      result.push(array.slice(index, index + 2))
+  const getGroups = async (stream) => {
+    try {
+      const groupInfo = await redis.xinfo("GROUPS", stream)
+      const hasGroup = groupInfo.some((g) => g[1] === CONSUMERGROUP)
+      return hasGroup ? true : createGroup(stream)
+    } catch (e) {
+      return createGroup(stream)
     }
-    return result
-  }, [])
+  }
 
   const processStreamKey =
     (fn) =>
     ([key, data]) => {
       const [streamId, ...fields] = data
-      const kv = R.fromPairs(arrayToPairs(fields[0]))
+      const kv = R.fromPairs(
+        fields[0].reduce((result, _, index, array) => {
+          if (index % 2 === 0) {
+            result.push(array.slice(index, index + 2))
+          }
+          return result
+        }, [])
+      )
       console.log("data", key, streamId, kv)
       fn(key, streamId, kv)
       return redis.xack(key, CONSUMERGROUP, streamId)
@@ -58,7 +51,7 @@ export default (host = "localhost", port = 6379) => {
     client: redis,
     addStreams,
     addNewStream(type, name) {
-      return map(getGroups)(addStreams(type, name))
+      return Promise.all(addStreams(type, name).map(getGroups))
     },
     addListener(type, name, status) {
       STREAMS = [...STREAMS, `${type}:${name}:${status}`]
