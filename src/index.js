003bf1c5ae509f6a80255fd8a66e2ea96d342919
Alisson <thelinuxlich@gmail.com>
2021-10-04 18:04:52 -0300

    use esm version of rubico

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Redis from "ioredis"
 import * as R from "ramda"
-import { map, tryCatch, pipe, reduce, curry, filter } from "rubico"
+import { map, tryCatch, pipe, reduce, curry, filter } from "rubico/es"
 
 export default (host = "localhost", port = 6379) => {
   const redis = new Redis(port, host)
